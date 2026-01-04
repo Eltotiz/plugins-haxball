8d66c69c131ce69ee5d85ab8999dfad0aeebf669
TotizDev <77215699+Eltotiz@users.noreply.github.com>
2026-01-03 23:41:11 -0300

    Update saludo.js

--- src/saludo.js
+++ src/saludo.js
@@ -1,9 +1,18 @@
 // saludo.js - Plugin simple de bienvenida
 
-var room = HBInit();  // Crea el room object (parámetros vacíos, HHM los ignora)
+var room = HBInit();  // Obligatorio: crea el room object
+
+// Opcional: metadata del plugin (recomendado para que aparezca bien en logs)
+room.pluginSpec = {
+  name: 'saludo',          // nombre único
+  author: 'Eltotiz',
+  version: '1.0.0',
+  config: {},              // si quieres config en el futuro
+  dependencies: [],        // sin dependencias
+  order: {},
+  incompatible_with: []
+};
 
 room.onPlayerJoin = function(player) {
   room.sendAnnouncement(`¡HOLA ${player.name}! 👋`, null, 0xFFFFFF, "bold", 1);
 };
-
-module.exports = room;  // ¡¡¡ESTO ES OBLIGATORIO!!!
