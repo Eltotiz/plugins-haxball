4eab234b2d3150a7e480414d9590f0428cd53a87
TotizDev <77215699+Eltotiz@users.noreply.github.com>
2026-01-03 23:19:41 -0300

    Update saludo.js

--- src/saludo.js
+++ src/saludo.js
@@ -1,5 +1,6 @@
-module.exports = function(room, config) {
+// Plugin de saludo simple - retorna la función directamente
+function(room, config) {
   room.onPlayerJoin = function(player) {
     room.sendAnnouncement(`¡HOLA ${player.name}! 👋`, null, 0xFFFFFF, "bold", 1);
   };
-};
+}
